90bdcee6119b8405eb48b24f28ed517b1f256203
wanghan <minglongoc@sina.com>
2017-05-09 09:39:36 +0800

    20170509_01

--- config/constant.php
+++ config/constant.php
@@ -5,12 +5,12 @@ return [
     //自定义常量
 
     //ivr返回的录音文件存放路径
-    'voice_path'=>'/VoiceAndModel/Voice/',
+    'voice_path'=>'VoiceAndModel/Voice/',
 
     //ivr返回的录音模型文件存放路径
-    'model_path'=>'/VoiceAndModel/Model/',
+    'model_path'=>'VoiceAndModel/Model/',
 
     //删除的录音文件
-    'voice_remove_path'=>'/VoiceAndModel/Tmp/'
+    'voice_remove_path'=>'VoiceAndModel/Tmp/'
 
 ];
